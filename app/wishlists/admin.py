58350a447cc71b03e2a44a1e550f6ce69620cb2c
woohaen88 <woohaen88@gmail.com>
2023-04-11 12:56:39 +0000

    Model: WishList

--- app/wishlists/admin.py
+++ app/wishlists/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from wishlists.models import WishList
 
-# Register your models here.
+
+@admin.register(WishList)
+class WishListAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "user",
+        "created_at",
+        "updated_at",
+    ]
--- app/wishlists/models.py
+++ app/wishlists/models.py
@@ -1,3 +1,38 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
+from django.conf import settings
+from common.models import CommonModel
 
-# Create your models here.
+
+class WishList(CommonModel):
+    """WisthList
+
+    Args:
+        required:
+            name: str
+        relation:
+            [MtoM] rooms : "rooms.Room"
+            [MtoM] experiences : "experiences.Experience"
+            [FK] user : settings.AUTH_USER_MODEL
+    """
+
+    name = models.CharField(
+        _("Name"),
+        max_length=150,
+    )
+    rooms = models.ManyToManyField(
+        "rooms.Room",
+        verbose_name=_("Rooms"),
+    )
+    experiences = models.ManyToManyField(
+        "experiences.Experience",
+        verbose_name=_("Experience"),
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_("USER"),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return self.name
