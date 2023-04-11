cb3b8f3b0d049967ea150fb6fa6d0e415fccba7c
woohaen88 <woohaen88@gmail.com>
2023-04-11 13:12:21 +0000

    Model: Bookings

--- app/bookings/admin.py
+++ app/bookings/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from bookings.models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    # :
+    #     required:
+    #         kind: ["room", "experience"]
+    #         guests: int
+    #     optional:
+    #         check_in: date
+    #         check_out: date
+    #         experience_time: datetime
+
+    #     relation:
+    #         [FK] user: settings.AUTH_USER_MODEL
+    #         [FK] room: "room.Room"
+    #         [FK] experience: "experiences.Experience"
+    list_display = [
+        "kind",
+        "guests",
+        "user",
+        "room",
+        "experience",
+    ]
+
+    list_filter = [
+        "kind",
+        "user",
+        "room",
+        "experience",
+    ]
--- app/bookings/models.py
+++ app/bookings/models.py
@@ -1,3 +1,83 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
+from django.conf import settings
+from common.models import CommonModel
 
-# Create your models here.
+
+class Booking(CommonModel):
+    """Booking
+
+    Args:
+        required:
+            kind: ["room", "experience"]
+            guests: int
+        optional:
+            check_in: date
+            check_out: date
+            experience_time: datetime
+
+        relation:
+            [FK] user: settings.AUTH_USER_MODEL
+            [FK] room: "room.Room"
+            [FK] experience: "experiences.Experience"
+    """
+
+    class BookingKindChoices(models.TextChoices):
+        ROOM = "room", _("ROOM")
+        EXPERIENCE = "experience", _("Experience")
+
+    kind = models.CharField(
+        _("Kind"),
+        max_length=15,
+        choices=BookingKindChoices.choices,
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_("User"),
+        on_delete=models.CASCADE,
+        related_name="bookings",
+    )
+    room = models.ForeignKey(
+        "rooms.Room",
+        verbose_name=_("Room"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        verbose_name=_("Experience"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    check_in = models.DateField(
+        _("Check In"),
+        auto_now=False,
+        auto_now_add=False,
+        null=True,
+        blank=True,
+    )
+    check_out = models.DateField(
+        _("Check Out"),
+        auto_now=False,
+        auto_now_add=False,
+        null=True,
+        blank=True,
+    )
+
+    experience_time = models.DateTimeField(
+        _("Experience Time"),
+        auto_now=False,
+        auto_now_add=False,
+        null=True,
+        blank=True,
+    )
+
+    guests = models.PositiveIntegerField(_("Guests"))
+
+    def __str__(self) -> str:
+        return f"{self.kind.title()} / {self.user}"
